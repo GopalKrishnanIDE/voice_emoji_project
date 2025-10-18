2ac0b870e6079e3e5df707f89f987a6a576db00f
GopalKrishnanIDE <gopal15031990@gmail.com>
2025-10-18 18:29:03 +0000

    Add POST method to RecordingView

--- recordings/urls.py
+++ recordings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RecordingView
 
 urlpatterns = [
-    path("recordings/", RecordingView.as_view(), name="recordings"),
+    path('', RecordingView.as_view(), name="recordings"),  # empty string, not "recordings/"
 ]
