cc1b3ccc22492ea6281f071a08ad073ccb4753fb
GopalKrishnanIDE <gopal15031990@gmail.com>
2025-10-18 15:23:33 +0000

    Add root URL view to prevent 404

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from .views import home  # import the new view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('recordings.urls')),  # your API app
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('recordings.urls')),
+    path('', home),  # root path
+]
