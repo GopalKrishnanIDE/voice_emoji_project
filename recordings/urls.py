250225f252f34749f0c594276b11c4e1dbfaa071
GopalKrishnanIDE <gopal15031990@gmail.com>
2025-10-18 15:03:35 +0000

    Fix 404 on root path and configure API route

--- recordings/urls.py
+++ recordings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RecordingView
 
 urlpatterns = [
-    path('recordings/', RecordingView.as_view(), name='recordings'),
+    path('record/', RecordingView.as_view(), name='recording'),
 ]
